f14342434fd28725a8e7928db1f99b8ad7d13852
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-02-18 14:08:02 +0100

    added main project markup

---
+++ app.js
@@ -0,0 +1,18 @@
+// Pytania do projektu 
+
+// 1. 
+// 2. 
+
+
+// Kroki do wykonania 
+
+// 1. Stworzenie markup w html 
+// 2. Po wpisaniu w inpucie link/tekst i ustawieniu czas to ma sie wyświetlić poniżej w liście 
+// 3. Napisanie funkcjonalnosci do głównego licznika , plus guzik start stop reset
+// 4. Połączenie ustawionego czasu z inputa  a nasz licznik - czyli 
+//         ustaliliśmy czas w inpucie 00:10 to wtedy kiedy licznik osiągnie 00:10 to ma nam wyświetlić obrazek 
+
+
+
+
+
