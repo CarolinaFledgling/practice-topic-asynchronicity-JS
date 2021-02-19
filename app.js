b5e92294afd5c2f35ac510587235d32700c854be
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-02-19 15:33:35 +0100

    added delete function , after clicking btn usun we delete our element

--- app.js
+++ app.js
@@ -24,47 +24,65 @@ const alertInfo = document.querySelector('.alert-info')
 
 
 
+// Tworzenie i spr elementu w naszej Liscie /wpisany tekst/ wybrany czas/ guzik usun
 const addNewLink = () => {
 
-    if (inputTeskt.value !== '') {
-        // dla wpisanego tekstu 
-        // const newLinkTekst = document.createElement('li')
-        // newLinkTekst.innerText = inputTeskt.value;
-        // ulList.appendChild(newLinkTekst);
+    if (inputTeskt.value !== '' && inputSetTime.value !== '') {
         creatDiv();
-
         alertInfo.innerText = '';
         inputTeskt.value = '';
-
-
     } else {
-        alertInfo.innerText = " Alert ! Wpisz jakiś tekst"
-
+        alertInfo.innerText = " Alert ! Wpisz jakiś tekst i wybierz czas"
     }
 
 }
 
 function creatDiv() {
-    // tworze diva który będzie przechowywał  guzik do usunięcia i nasz ustawiony czas
+    // tworze diva który będzie przechowywał  wpisany tekst /guzik do usunięcia i nasz ustawiony czas
+  
     const panelDiv = document.createElement('div');
-    panelDiv.classList.add('panel')
-    ulList.appendChild(panelDiv)
+    panelDiv.classList.add('panel');
+    ulList.appendChild(panelDiv);
 
-    const newLinkTekst = document.createElement('li')
+    const newLinkTekst = document.createElement('li');
     newLinkTekst.innerText = inputTeskt.value;
 
     const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('btnDelete');
     deleteBtn.textContent = 'usuń';
 
-    const newLinkTime = document.createElement('p')
+    const newLinkTime = document.createElement('p');
     newLinkTime.innerText = inputSetTime.value;
 
-    panelDiv.appendChild(newLinkTekst)
-    panelDiv.appendChild(newLinkTime)
-    panelDiv.appendChild(deleteBtn)
+    panelDiv.appendChild(newLinkTekst);
+    panelDiv.appendChild(newLinkTime);
+    panelDiv.appendChild(deleteBtn);
 
 
 
 }
 
-btnAdd.addEventListener('click', addNewLink)
+// Usunięcie elementu z naszej listy 
+
+function deleteElement(e) {
+    // nasze e to event który przechwytamy , tutaj e to nasz Click / ,puse event 
+    // sprawdzamy czy najbliższy button elementu w który kliknęliśmy zawiera klase btnDelete
+    if (e.target.closest('button').classList.contains('btnDelete')) {
+        console.log(e)
+        console.log(e.target)
+        console.log(e.target.closest('div'))
+        const deleteElement = e.target.closest('div')
+        deleteElement.remove();
+    }
+}
+
+
+
+btnAdd.addEventListener('click', addNewLink);
+ulList.addEventListener('click', deleteElement)
+
+
+
+
+
+// Timer 
