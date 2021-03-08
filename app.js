dad26e10a6c9bba981dae6102b4a1a09a78b3bdf
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-03-08 08:20:03 +0100

    added changes after PR from mentor

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-  const inputTeskt = document.querySelector('.input__tekst');
+  const inputOurText = document.querySelector('.input__tekst');
   const inputSetTime = document.querySelector('.time');
   const btnAdd = document.querySelector('.btnPlus');
   const ulList = document.querySelector('.ul-list');
@@ -8,31 +8,11 @@
   let timeFromInput = null;
 
 
-  // na ostatnich zajeciach pokazywałes żeby sobie tworzyć coś takiego jak eventList, ale nie wiem jak to zastosować żeby działało, może dałoby sie to pokazać jeszcze raz ale na całym przykładzie ktory będzie to realizował , jeśli to nie problem 
-  // pokazałeś coś takiego poniżej : hmm wiem że to jest tablica z 3 obiektami 
 
-  //   const eventList = [{
-  //           time: 500,
-  //           url: 'http://google.com/image.png',
-  //           loaded: true
-  //       },
-  //       {
-  //           time: 400,
-  //           url: 'http://google.com/image.png',
-  //           loaded: true
-  //       },
-  //       {
-  //           time: 300,
-  //           url: 'http://google.com/image.png',
-  //           loaded: true
-  //       },
-  //   ]
 
 
   function delay(time = 0) {
-      return new Promise(function (resolve, reject) {
-          setTimeout(resolve, time)
-      })
+      return new Promise((resolve, reject) => setTimeout(resolve, time))
   }
 
   function createImage(value) {
@@ -46,7 +26,7 @@
       const panelDiveFromCreatList = creatListElement()
       delay(timeFromInput)
           .then(() => {
-              createImage(inputTeskt.value)
+              createImage(inputOurText.value)
               panelDiveFromCreatList.prepend(img)
           })
           .catch((err) => console.log(err, 'mamy błąd'))
@@ -55,13 +35,13 @@
 
   function creatListElement() {
       // tworze diva który będzie przechowywał  wpisany tekst /guzik do usunięcia i nasz wpisany czas 
-      if (inputTeskt.value !== '' && inputSetTime.value !== '') {
+      if (inputOurText.value !== '' && inputSetTime.value !== '') {
           const panelDiv = document.createElement('div');
           panelDiv.classList.add('panel');
           ulList.appendChild(panelDiv);
 
-          const UrlLink = document.createElement('li');
-          UrlLink.innerText = inputTeskt.value;
+          const urlLink = document.createElement('li');
+          urlLink.innerText = inputOurText.value;
 
           const deleteBtn = document.createElement('button');
           deleteBtn.classList.add('btnDelete');
@@ -72,8 +52,8 @@
           timeFromInput = inputSetTime.value;
 
           console.log(timeFromInput)
-          
-          panelDiv.appendChild(UrlLink);
+
+          panelDiv.appendChild(urlLink);
           panelDiv.appendChild(ourSetTime);
           panelDiv.appendChild(deleteBtn);
 
@@ -116,22 +96,21 @@
   const btnStartCounter = document.querySelector('.btnStart');
   const btnStopCounter = document.querySelector('.btnStop');
   const btnResetCounter = document.querySelector('.btnReset');
-  const counterTime = document.querySelector('.counter__time')
+  const jsCounterTime = document.querySelector('.counter__time')
   let number = 0;
   let idSetInterval = false;
 
   function startTimer() {
-      idSetInterval = true; // to blokuje  ponownie kliknac bo handleClick, zamieni sie na false i nie wchodzi do srodka wiec nie odpala nam ponownie startTimer()
       idSetInterval = setInterval(() => {
           number++;
-          counterTime.textContent = number;
+          jsCounterTime.textContent = number;
           console.log(number)
 
       }, 1000)
 
   }
 
-  naszCzasZTimera = number;
+  ourTimeFromCounter = number;
 
   function stopTimer() {
       clearInterval(idSetInterval)
@@ -146,7 +125,7 @@
 
   function resetClick(e) {
       number = 0;
-      counterTime.textContent = 0;
+      jsCounterTime.textContent = 0;
       clearInterval(idSetInterval);
       idSetInterval = false;
 
